316f631db189cb55591106320e3c17da8d587005
Marc365 <marc365@github.com>
2021-12-10 09:02:12 +0000

    1.5 update
    removed "File not specified" error message, replaced with a call to the file selector
    removed repeated library load error message
    added 320kbps mode for mp3
    added continous record if you change the filename while recording it will continue recording starting a new file
    fixed stuck processes when using exit before using stop
    added an error message that reports the full error if the recording was stopped with an error - this is for further debugging if the audio is dropped due to driver output changes eg. headphones
    created custom dialog for Overwrite Append Cancel
    added Dated filename mode
    
     Changes:
    	new file:   Form2.Designer.cs
    	new file:   Form2.cs
    	new file:   Form2.resx

---
+++ Form2.cs
@@ -0,0 +1,61 @@
+﻿#region MIT license
+// 
+// MIT license
+//
+// Copyright (c) 2017 Marc Williams
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+#endregion
+using Microsoft.Win32;
+using NAudio.Lame;
+using NAudio.Wave;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DesktopRecorder
+{
+    public partial class Form2 : Form
+    {
+        /// <summary>
+        /// Initializer
+        /// </summary>
+        public Form2()
+        {
+            InitializeComponent();
+            
+            button1.Text = "Overwrite";
+            button2.Text = "Append";
+            button3.Text = "Cancel";
+
+            label1.Text = string.Format("Overwrite the file.{0}Append recording at the end of the file{0}Cancel starting the recording", Environment.NewLine);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Result = ((Button)sender).Text;
+            Close();
+        }
+
+        public string Result { get; set;}
+    }
+}
